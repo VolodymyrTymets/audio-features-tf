6ddaaf309d773f0141b43424d3bfc8057a636d1c
vova <volodymyrtymets@gmail.com>
2026-01-13 17:15:52 +0200

    fix

--- src/neural_network/strategy/strategies/stft_strategy.py
+++ src/neural_network/strategy/strategies/stft_strategy.py
@@ -3,7 +3,7 @@ from src.neural_network.strategy.strategies.base_strategy import BaseStrategy
 from src.audio_features.types import AFTypes
 
 
-class MelStrategy(BaseStrategy):
+class STFTStrategy(BaseStrategy):
   def __init__(self, sr: int, frame_length: int, hop_length: int):
     super(self.__class__, self).__init__(sr, frame_length, hop_length)
     self.af_type = AFTypes.stft
