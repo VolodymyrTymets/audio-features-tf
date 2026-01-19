314eac7a63c4e222d72cbfc98a901d977c99adff
vova <volodymyrtymets@gmail.com>
2026-01-19 14:25:50 +0200

    refactor

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ import librosa
 from src.audio_features.types import AFTypes
 from src.neural_network.train import train
 from src.neural_network.valid_record import valid_record
-# from src.neural_network.filter_data_set import filter_data_set
 
 def main():
-  audio_features = [AFTypes.sc]
+  audio_features = [AFTypes.wave, AFTypes.fft, AFTypes.stft, AFTypes.mfcc, AFTypes.mel, AFTypes.bw, AFTypes.sc, AFTypes.ae, AFTypes.rms, AFTypes.zcr, AFTypes.ber]
   for af_type in audio_features:
     print("Starting training...")
     train(af_type, save_af=True)
@@ -14,8 +13,6 @@ def main():
     print("Starting validation of record...")
     valid_record(af_type, show_plot=True)
     print("End validation of record.")
-
-    # filter_data_set(af_type, data_set_name='data_set_0.2_filtered')
 
 if __name__ == "__main__":
   main()
