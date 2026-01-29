3596b94cfae662c6dd4df29a563d45a36ddf95d5
vova <volodymyrtymets@gmail.com>
2026-01-29 14:19:17 +0200

    refactor

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.neural_network.valid_record import valid_record
 
 def main():
   train(af_type=AFTypes.mel, model_type=ModelTypes.CNN ,  save_af=False)
-  # valid_record(af_type=AFTypes.mel, show_plot=True)
+  valid_record(af_type=AFTypes.mel, show_plot=True)
 
 if __name__ == "__main__":
   main()
