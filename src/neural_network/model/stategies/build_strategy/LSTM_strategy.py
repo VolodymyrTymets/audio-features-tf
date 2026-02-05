4910b08fb7ce525bc21f8269d7cf24046ed9c0ab
vova <volodymyrtymets@gmail.com>
2026-02-05 10:14:42 +0200

    adjust LSTM

--- src/neural_network/model/stategies/build_strategy/LSTM_strategy.py
+++ src/neural_network/model/stategies/build_strategy/LSTM_strategy.py
@@ -8,9 +8,24 @@ from src.neural_network.model.stategies.build_strategy.build_strategy_interface 
 
 class LSTMModelBuildStrategy(IModelBuildStrategy):
   def build(self, input_shape: np.ndarray, output_shape: int, train_ds):
+    # # Instantiate the `tf.keras.layers.Normalization` layer.
+    # norm_layer = layers.Normalization()
+    # # Fit the state of the layer to the spectrograms
+    # # with `Normalization.adapt`.
+    # norm_layer.adapt(data=train_ds.map(
+    #   map_func=lambda spec, label: spec))
+
     model = models.Sequential([
-      layers.LSTM(124, input_shape=input_shape, return_sequences=True),
+      # layers.Input(shape=input_shape),
+      # Downsample the input.
+      # layers.Resizing(32, 32),
+      # # Normalize.
+      # norm_layer,
+      layers.LSTM(32, input_shape=input_shape, return_sequences=True, activation='relu'),
       layers.LSTM(64),
+      layers.Dropout(0.25),
+      # # Flatten the result to feed into DNN
+      layers.Flatten(),
       layers.Dense(64, activation='relu'),
       layers.Dropout(0.5),
       layers.Dense(output_shape, activation='softmax'),
